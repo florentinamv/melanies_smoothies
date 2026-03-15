3d8e03cdfd8a7dc2ed41afe1623bc8933e037eab
florentinamv <62209123+florentinamv@users.noreply.github.com>
2026-03-15 13:31:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,8 @@ if ingredients_list:
 
     for f in ingredients_list:
         ingredients_string+=f+' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
     st.write(ingredients_string)
 
@@ -42,7 +44,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
